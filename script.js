f30cca76496b826b8f195c4ac39fada9260e0a9a
TheBooksTable <86239307+TheBooksTable@users.noreply.github.com>
2025-09-15 01:11:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ let allEpisodes = [];
 const SURPRISE_LIST = [
   'Breaking Bad','The Office','Friends','SpongeBob SquarePants',
   'Rick and Morty','Stranger Things','The Simpsons','Archer',
-  'Seinfeld','Avatar: The Last Airbender'
+  'Seinfeld','Avatar: The Last Airbender', 'Smiling Friends', 
+  'BoJack Horseman', 'Black Mirror', 'Brooklyn Nine-Nine', 
+  'Supernatural', 'South Park', 'Twin Peaks','Fargo', 
+  'Better Call Saul','Smiling Friends', 'Infinity Train', 
+  'Kipo and the Age of Wonderbeasts', 'Tuca & Bertie', 
+  'Final Space', 'The Midnight Gospel', 'Undone', 
+  'Close Enough', 'Hilda', 'F Is for Family', 
+  'Solar Opposites', 'Golan the Insatiable', 
+  'Adventure Time: Distant Lands', 'Over the Garden Wall', 
+  'Primal', 'Magical Girl Raising Project', 'Welcome to the Ballroom', 
+  'Aggretsuko', 'Bee and PuppyCat', 'King Star King', 
+  'The Shivering Truth', '12 oz. Mouse', 'The Brak Show', 
+  'Drawn Together', 'Superjail!'
 ];
 
 function loadFavorites(){
@@ -200,3 +212,4 @@ renderFavorites();
     });
   }
 })();
+
